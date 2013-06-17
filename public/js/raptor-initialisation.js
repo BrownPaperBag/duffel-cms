a5df7cae75f5644952be9ec611d4ecb16c498161
Michael Robinson <mike@pagesofinterest.net>
2013-06-18 01:10:25 +1200

    Raptor initialisation updated

--- public/js/raptor-initialisation.js
+++ public/js/raptor-initialisation.js
@@ -3,6 +3,18 @@ $(function() {
     plugins: {
       dock: {
         docked: true
+      },
+      save: {
+        plugin: 'saveRest'
+      },
+      saveRest: {
+        url: '/cms/admin/content',
+        data: function(html) {
+          return {
+            id: this.raptor.getElement().data('id'),
+            content: html
+          };
+        }
       }
     }
   });
