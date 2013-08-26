1f87eb2f558fc11c288ae3766f90b8118f83d8e8
Michael Robinson <mike@pagesofinterest.net>
2013-08-26 19:51:34 +1200

    Use initialise instead of doing it all in index

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-var express = require('express'),
-  nunjucks = require('nunjucks');
-
-var cmsblock = require('./lib/nunjucks-tags/cmsblock'),
-  cmstag = require('./lib/nunjucks-tags/cmstag');
-
 module.exports = {
-  initialise: function(app) {
-    var nunjucksEnvironment = app.get('nunjucksEnvironment');
-    nunjucksEnvironment.addExtension('CMSBlock', cmsblock);
-    nunjucksEnvironment.addExtension('CMSTag', cmstag);
-
-    app.use('/cms', express.static(__dirname + '/public'));
-  }
+  initialise: require('./lib/initialise')
 };
