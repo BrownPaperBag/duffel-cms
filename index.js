90cad893995250e50034db619314b92d36e9ffd3
Michael Robinson <mike@pagesofinterest.net>
2013-08-26 23:37:26 +1200

    Make CMS models available to outside world

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 module.exports = {
-  initialise: require('./lib/initialise')
+  initialise: require('./lib/initialise'),
+  CMSBlock: function() {
+    return require('./lib/models/CMSContent').model();
+  },
+  CMSRequest: function() {
+    return require('./lib/models/CMSRequest').model();
+  }
 };
